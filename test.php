bca56d91caa0fa6fb2c3e4a646b342904cf56563
Petar Prodanovic <petar.prodanovic@gmail.com>
2017-12-02 23:30:08 +0100

    no message

--- test.php
+++ test.php
@@ -4,33 +4,48 @@
  * User: petar
  * Date: 23.11.2017
  * Time: 16:28
- */
-;
-$tmpArray = array(1,5,4,7,8,11);
-$numMatch = array();
+ */;
+$arrayofmatches = array();
+$numofteams = 16;
+$matchnumber = 0;
+$matchnumberperround = array();
+$matchnumberend = array(0);
+$round = 1;
+$numofrounds = log($numofteams)/ log(2);
 
+//function matchgenerator($numofteams){
+for ($i = 1; $i <= $numofteams; $i++) {
+//    while ($matchnumber < $numofteams) {
+        if(fmod($i, 2) == 1){
+            $matchnumber ++;
+            $arrayofmatches[$matchnumber][0] = $i;
+        } else {
+            $arrayofmatches[$matchnumber][1] = $i;
 
-function matchgenerator($array){
-    $numparticipants = count($array);
-    $maxnumMatches = ceil($numparticipants/2);
-    $maxturnamentParticipants=0;
-    $tmpcount = 0;
-    while ($numparticipants > $maxturnamentParticipants) {
-        $maxturnamentParticipants = pow(2,$tmpcount);
-        $tmpcount++;
-    }
-
-    $freeparticipants = $maxturnamentParticipants - $numparticipants;
-
-    echo "$maxnumMatches - $maxturnamentParticipants ($freeparticipants)<br>";
-    for($i=1;  $i<=$maxnumMatches; $i++ ) {
-        $home = $i*2-2;
-        $away = $i*2-1;
-        echo "mec broj $i igraju $array[$home] - $array[$away]<br>";
-    }
+        }
+//    }
 
 
 }
+//print_r($arrayofmatches);
+
+$matchnumberperround[1] = $matchnumber;
+$matchnumberend[1] = $matchnumber;
+
+for($r = 2; $r<= $numofrounds; $r++) {
+    $matchnumberperround[$r] = $matchnumberperround[$r-1]/2;
+    for ($m=$matchnumberend[$r-2]+1;$m<=$matchnumberend[$r-1]; $m++){
+        $matchnumber++;
+        $val = $matchnumberend[$r-2] + $m*2;
+        $arrayofmatches[$matchnumber][0] = $m;
+        $m++;
+        $arrayofmatches[$matchnumber][1] = $m;
+
+    }
+
+    $matchnumberend[$r] = $matchnumber;
+}
 
 
-matchgenerator($tmpArray);
+print_r($arrayofmatches);
+echo"<br>";
